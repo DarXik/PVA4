c76986b7305b050ed4474dd77ed13e501a16090a
DarXik <davca749@gmail.com>
2024-02-18 10:10:08 +0100

    delegates

--- Del_Events/Program.cs
+++ Del_Events/Program.cs
@@ -7,46 +7,49 @@ namespace Del_Events
     {
         public static void Main(string[] args)
         {
-            Seznam iSeznam = new Seznam();
+            Seznam seznamObj = new Seznam();
 
-            iSeznam.EventAdd += new AddHandler(Pridej); //připojíme k události EventHandler (delegát nám ho přenese k události)
-            iSeznam.EventRemove += new RemoveHandler(Odeber); //připojíme k události EventHandler (delegát nám ho přenese k události)
+            seznamObj.EventAdd += Pridej; //připojíme k události EventHandler (delegát nám ho přenese k události)
+            seznamObj.EventRemove += Odeber; //připojíme k události EventHandler (delegát nám ho přenese k události)
 
-            iSeznam.Add("karel"); //Protože jsme zavolali metodu Add vyvolá se událost a ta zavolá EventHandler Pridej
-            iSeznam.Remove("karel"); //Protože jsme zavolali metodu Remove vyvolá se událost a ta zavolá EventHandler Odeber
+            seznamObj.Add("karel"); //Protože jsme zavolali metodu Add vyvolá se událost a ta zavolá EventHandler Pridej
+            seznamObj.Remove("karel"); //Protože jsme zavolali metodu Remove vyvolá se událost a ta zavolá EventHandler Odeber
 
-            ///
-
-
-            iSeznam.Add("michal");
+            seznamObj.Add("michal");
         }
 
-        static void Pridej(string jmeno)//EventHanlder - metoda která se stane, když se vyvolá událost
+        static void Pridej(string jmeno) //EventHanlder - metoda která se stane, když se vyvolá událost
         {
             Console.WriteLine(jmeno + " bylo přídáno");
         }
-        static void Odeber(string jmeno)//EventHanlder - metoda která se stane, když se vyvolá událost
+
+        static void Odeber(string jmeno) //EventHanlder - metoda která se stane, když se vyvolá událost
         {
             Console.WriteLine(jmeno + " bylo odebráno");
         }
     }
+
     public delegate void AddHandler(string jmeno); //Delegát - přenáší jako argument celou metodu (Event Handler), musí mít jako argument vše co metoda ve které se spouští
-    public delegate void RemoveHandler(string jmeno);  //Delegát - přenáší jako argument celou metodu (Event Handler), musí mít jako argument vše co metoda ve které se spouští
+
+    public delegate void RemoveHandler(string jmeno); //Delegát - přenáší jako argument celou metodu (Event Handler), musí mít jako argument vše co metoda ve které se spouští
+
     class Seznam
     {
         public event AddHandler EventAdd; //Event je nějaká událost, která se někdy stane v průběhu toku programem
         public event RemoveHandler EventRemove;
 
-        private List<string> seznam = new List<string>();
+        private List<string> seznamJmen = new List<string>();
+
         public void Add(string jmeno)
         {
-            seznam.Add(jmeno);
-            EventAdd(jmeno); //Tady se event vyvolá - musí obsahovat stejný argument jako metoda
+            seznamJmen.Add(jmeno);
+            EventAdd?.Invoke(jmeno); //Tady se event vyvolá - musí obsahovat stejný argument jako metoda
         }
+
         public void Remove(string jmeno)
         {
-            seznam.Remove(jmeno);
-            EventRemove(jmeno); //Tady se event vyvolá - musí obsahovat stejný argument jako metoda
+            seznamJmen.Remove(jmeno);
+            EventRemove?.Invoke(jmeno); //Tady se event vyvolá - musí obsahovat stejný argument jako metoda
         }
     }
 }
---
+++ Delegates_practice/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Delegates_practice
+{
+    public delegate void myDelegate();
+    public delegate int MathOperator(int x, int y); // odkazuje na metody které berou 2 int parametry a vracejí int
+
+    public delegate void methodHolder();
+
+    Action<object> david;
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var obj1 = new MathOp();
+            obj1.myEvent += ShowDiff;
+            // obj1.myEvent += () => Console.WriteLine("Diff has changed");
+
+            // for (int i = 0; i < 4; i++)
+            // {
+            //     Console.WriteLine("Result: " + obj1.Add(1, i));
+            //     Thread.Sleep(1000);
+            // }
+
+            Console.WriteLine("Substract: " + obj1.Subtract(1, 100));
+
+            MathOperator multiplyDelegate = Multiply; // vytvoří instance s odkazem na delegáty
+
+            int result = multiplyDelegate(1, 2); // použije delegáta pro zavolaní dané metody Add(1, 2)
+
+            Console.WriteLine("Result3: " + result);
+
+            PerformOperation(multiplyDelegate, 10, 4);
+
+
+            List<methodHolder> list = new List<methodHolder>();
+            methodHolder method1 = Method1;
+            methodHolder method2 = Method2;
+            methodHolder method3 = Method3;
+            methodHolder method4 = Method4;
+
+            list.Add(method1);
+            list.Add(method2);
+            list.Add(method3);
+            list.Add(method4);
+
+            int input = Convert.ToInt32(Console.ReadLine()) - 1;
+
+            list[input].Invoke();
+        }
+
+        private static void Method1()
+        {
+            Console.WriteLine("Method1 has been called");
+        }
+        private static void Method2()
+        {
+            Console.WriteLine("Method2 has been called");
+        }
+        private static void Method3()
+        {
+            Console.WriteLine("Method3 has been called");
+        }
+        private static void Method4()
+        {
+            Console.WriteLine("Method4 has been called");
+        }
+
+
+        private static void PerformOperation(MathOperator operation, int x, int y)
+        {
+            int result = operation(x, y);
+            Console.WriteLine("Result4: " + result);
+        }
+
+        private static void ShowDiff()
+        {
+            Console.WriteLine("--Event has happened--");
+        }
+
+        private static int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+    }
+
+    class MathOp
+    {
+        public event myDelegate myEvent;
+
+        public int Add(int x, int y)
+        {
+            myEvent?.Invoke();
+            return x + y;
+        }
+
+        public int Subtract(int x, int y)
+        {
+            myEvent?.Invoke();
+            return x - y;
+        }
+    }
+}
