b6801d4d8b0486735472a3a630680b99b822905e
DarXik <73177923+DarXik@users.noreply.github.com>
2024-02-29 17:33:26 +0100

    pva

---
+++ BigInteger/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Numerics;
+
+var rnd = new Random();
+for (int i = 0; i < 1000000; i++)
+{
+    var big = (BigInteger) Math.Pow(10, i);
+    Console.WriteLine(big);
+}
---
+++ events_23-2/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace events_23_2
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    // ReSharper disable once InconsistentNaming
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var bulb = new Lightbulb();
+
+            while (true)
+            {
+                Console.WriteLine("Zadejte příkaz (on/off):");
+                var command = Console.ReadLine()?.ToLower();
+
+                if (command == "on")
+                {
+                    bulb.TurnOn();
+                    Console.WriteLine($"Stav žárovky: {bulb.GetState()}");
+                    bulb.SimulateShortCircuit();
+                }
+                else if (command == "off")
+                {
+                    bulb.TurnOff();
+                    Console.WriteLine($"Stav žárovky: {bulb.GetState()}");
+                    bulb.SimulateShortCircuit();
+                }
+                else
+                {
+                    Console.WriteLine("Neplatný příkaz.");
+                }
+            }
+        }
+    }
+
+    public delegate void eventHandler();
+
+    class Lightbulb
+    {
+        private bool isOn;
+        private bool isShortCircuited;
+        public event eventHandler IsShortCircuitedEventHandler;
+        public event eventHandler IsOnEventHandler;
+
+        public Lightbulb()
+        {
+            isOn = false;
+            isShortCircuited = false;
+        }
+
+        public void TurnOn()
+        {
+            isOn = true;
+            Console.WriteLine("Žárovka je zapnutá.");
+        }
+
+        public void TurnOff()
+        {
+            isOn = false;
+            Console.WriteLine("Žárovka je vypnutá.");
+        }
+
+        public string GetState()
+        {
+
+            if (isShortCircuited)
+            {
+                return "zkrat";
+            }
+            else if (isOn)
+            {
+                IsOnEventHandler?.Invoke();
+
+                return "svítí";
+            }
+            else
+            {
+
+                return "zhasnutá";
+            }
+        }
+
+        public void ShortCircuited()
+        {
+            isShortCircuited = true;
+            Console.WriteLine("Došlo ke zkratu!");
+        }
+
+        public void SimulateShortCircuit()
+        {
+            var random = new Random();
+            var randomNumber = random.Next(1, 10); // Náhodné číslo od 1 do 9
+
+            if (randomNumber == 5) // Zkrat nastane, pokud náhodné číslo je rovno 5
+            {
+                IsShortCircuitedEventHandler?.Invoke();
+            }
+            else
+            {
+                // dořešit zkrat, chci vyřešit, nejprve zhasnout žárovku, jinak zkrat zůstane
+            }
+        }
+    }
+}
