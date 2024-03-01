ed2e79c2e6850696a64bcd44e3adc335000d6d60
DarXik <73177923+DarXik@users.noreply.github.com>
2024-03-01 11:40:27 +0100

    1

--- events_23-2/Program.cs
+++ events_23-2/Program.cs
@@ -4,13 +4,22 @@ namespace events_23_2
 {
     // ReSharper disable once ClassNeverInstantiated.Global
     // ReSharper disable once InconsistentNaming
+
+    public delegate void eventHandler();
+
+    public delegate void eventHandler2(bool isOn);
+
     internal class Program
     {
+        public static event eventHandler IsShortCircuited;
+        public static bool IsOn = false;
+
         public static void Main(string[] args)
         {
             var bulb = new Lightbulb();
-            bulb.IsShortCircuitedEventHandler += () => Console.WriteLine("Zkrat nastal.");
-            bulb.IsOnEventHandler += () => Console.WriteLine("Žárovka je zapnutá.");
+            bulb.SwitchedState += SimulateShortCircuit;
+            IsShortCircuited += bulb.BulbShortCircuit;
+
             while (true)
             {
                 Console.WriteLine("Zadejte příkaz (on/off):");
@@ -18,57 +27,83 @@ namespace events_23_2
 
                 if (command == "on")
                 {
-                    bulb.TurnOn();
-                    bulb.SimulateShortCircuit();
+                    if (IsOn)
+                    {
+                        Console.WriteLine("Žárovka svítí :)");
+                    }
+                    else
+                    {
+                        IsOn = bulb.TurnOn();
+                    }
                 }
                 else if (command == "off")
                 {
-                    bulb.TurnOff();
-                    bulb.SimulateShortCircuit();
+                    if (!IsOn)
+                    {
+                        Console.WriteLine("Žárovka nesvítí :(");
+                    }
+                    else
+                    {
+                        IsOn = bulb.TurnOff();
+                    }
                 }
                 else
                 {
                     Console.WriteLine("Neplatný příkaz.");
                 }
             }
+        }
 
+        private static void SimulateShortCircuit(bool bulbState)
+        {
+            Console.WriteLine(bulbState ? "Žárovka je zapnutá." : "Žárovka je vypnutá.");
+
+            var random = new Random();
+            var randomNumber = random.Next(1, 10);
+
+            if (randomNumber == 5)
+            {
+                IsShortCircuited?.Invoke();
+            }
         }
     }
-
-    public delegate void eventHandler();
 
     class Lightbulb
     {
         private bool isOn;
-        public event eventHandler IsShortCircuitedEventHandler;
-        public event eventHandler IsOnEventHandler;
+        public event eventHandler2 SwitchedState;
 
         public Lightbulb()
         {
             isOn = false;
         }
 
-        public void TurnOn()
+        public void BulbShortCircuit()
+        {
+            Console.WriteLine("Žárovka praskla! ://");
+            Console.WriteLine("Tak ji prosím nejdříve vypni: ");
+            var input = Console.ReadLine();
+            while (input != "off")
+            {
+                Console.WriteLine("Nejdříve ji vypni!!! [off]");
+                input = Console.ReadLine();
+            }
+
+            Program.IsOn = false;
+        }
+
+        public bool TurnOn()
         {
             isOn = true;
-            IsOnEventHandler?.Invoke();
+            SwitchedState?.Invoke(isOn);
+            return isOn;
         }
 
-        public void TurnOff()
+        public bool TurnOff()
         {
             isOn = false;
-            Console.WriteLine("Žárovka je vypnutá.");
-        }
-
-        public void SimulateShortCircuit()
-        {
-            var random = new Random();
-            var randomNumber = random.Next(1, 10);
-
-            if (randomNumber == 5)
-            {
-                IsShortCircuitedEventHandler?.Invoke();
-            }
+            SwitchedState?.Invoke(isOn);
+            return isOn;
         }
     }
 }
