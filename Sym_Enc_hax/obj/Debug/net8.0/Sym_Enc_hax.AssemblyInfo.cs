bf077b58fde114d2c36a7a6e9846b45afdf24ef1
DarXik <davca749@gmail.com>
2024-03-08 11:51:16 +0100

    hax 2

--- Sym_Enc_hax/obj/Debug/net8.0/Sym_Enc_hax.AssemblyInfo.cs
+++ Sym_Enc_hax/obj/Debug/net8.0/Sym_Enc_hax.AssemblyInfo.cs
@@ -13,15 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sym_Enc_hax")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e920ba49a502a3b0b51c933374a3c1da60d5bed")]
->>>>>>> Stashed changes
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67bb4eac521a309bcc4aa00b76a0dd73bdc70bc7")]
->>>>>>> Stashed changes
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed2e79c2e6850696a64bcd44e3adc335000d6d60")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sym_Enc_hax")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sym_Enc_hax")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
