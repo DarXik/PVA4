54a731dbdc456e3155c5c37ee542788110531029
DarXik <73177923+DarXik@users.noreply.github.com>
2024-05-24 13:14:28 +0200

    maturita

---
+++ Test/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography;
+
+var randomNumber = new byte[4];
+
+// using - po skončení se zavolá Dispose - uvolnění zdroje
+using (var rng = RandomNumberGenerator.Create()) // nová instance třídy
+{
+    // generuje krypto. bezpečným způsobem náhodné byty a ukládá do pole
+    rng.GetBytes(randomNumber);
+}
+
+// Převedení celé byte pole na int, začne od prvního indexu v poli (0)
+// převede náhodné byty na náhohdné celé číslo
+int randomInt = BitConverter.ToInt32(randomNumber, 0);
+
+Console.WriteLine("Náhodné číslo: " + randomInt);
