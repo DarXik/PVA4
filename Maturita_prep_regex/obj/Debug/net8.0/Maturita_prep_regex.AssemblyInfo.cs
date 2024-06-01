4c27c26edc1128f5c681d063b931b83f50103f93
DarXik <73177923+DarXik@users.noreply.github.com>
2024-06-01 13:52:07 +0200

    maturita final DONE

--- Maturita_prep_regex/obj/Debug/net8.0/Maturita_prep_regex.AssemblyInfo.cs
+++ Maturita_prep_regex/obj/Debug/net8.0/Maturita_prep_regex.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Maturita_prep_regex")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c84474cdded891acfc0c3ff21bc3974120d443d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54a731dbdc456e3155c5c37ee542788110531029")]
 [assembly: System.Reflection.AssemblyProductAttribute("Maturita_prep_regex")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Maturita_prep_regex")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
